9d910d55b18e1f5e59d71645a059e0a0dc033c3b
Josh Schairbaum <joshua.schairbaum@gmail.com>
2009-12-09 09:56:05 -0600

    moved exit & about to a menu

--- src/com/jschairb/scorekeeper/ScoreKeeper.java
+++ src/com/jschairb/scorekeeper/ScoreKeeper.java
@@ -3,10 +3,13 @@ package com.jschairb.scorekeeper;
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+// import android.view.View;
+// import android.view.View.OnClickListener;
 
-public class ScoreKeeper extends Activity implements OnClickListener {
+public class ScoreKeeper extends Activity {// implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,21 +21,31 @@ public class ScoreKeeper extends Activity implements OnClickListener {
         // continueButton.setOnClickListener(this); 
         // View newButton = findViewById(R.id.new_button); 
         // newButton.setOnClickListener(this);
-        View aboutButton = findViewById(R.id.about_button); 
-        aboutButton.setOnClickListener(this);
-        View exitButton = findViewById(R.id.exit_button); 
-        exitButton.setOnClickListener(this);
     }
     
-    public void onClick(View v) {
-    	switch (v.getId()) {
-    	case R.id.about_button:
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+    	return true;
+    }
+    
+//    public void onClick(View v) {
+//    	switch (v.getId()) {
+//    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()) {
+    	case R.id.about_menu:
     		Intent i = new Intent(this, About.class);
     		startActivity(i);
-    		break;
-    	case R.id.exit_button:
+    		return true;
+    	case R.id.exit_menu:
     		finish();
-    		break;
+    		return true;
     	}
+    	return false;
     }
 }
